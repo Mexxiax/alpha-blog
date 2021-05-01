0079de7d477e441329c10647220dbdc2e44f208c
messias <miguelsoares91@gmail.com>
2021-05-01 18:12:59 +0100

    articles create and render

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        #render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect
+        @article.save
+        redirect_to @article
+    end
+
 end
