65395b4f281d56fd25d9ad85dcd904d44c4bc94d
messias <miguelsoares91@gmail.com>
2021-05-03 09:42:53 +0100

    bootstrap icons added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,50 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
 
-    def show; end
+  def show; end
 
-    def new
-        @article = Article.new #para carregar na criação de artigos
-    end
+  def new
+    @article = Article.new #para carregar na criação de artigos
+  end
 
-    def edit; end
-    
+  def edit; end
+  
 
-    def create
-        #render plain: params[:article]
-        #render plain: @article.inspect
-        @article = Article.new(article_params)
-        return render 'new' unless @article.save
+  def create
+    #render plain: params[:article]
+    #render plain: @article.inspect
+    @article = Article.new(article_params)
+    return render 'new' unless @article.save
 
-        flash[:notice] = "Article was created successfully"
-        redirect_to @article
-    end
-    
-    def update
-        return render 'edit' unless @article.update(article_params)
+    flash[:notice] = "Article was created successfully"
+    redirect_to @article
+  end
+  
+  def update
+    return render 'edit' unless @article.update(article_params)
 
-        flash[:notice] = "Article was edited successfully"
-        redirect_to @article
-    end
+    flash[:notice] = "Article was edited successfully"
+    redirect_to @article
+  end
 
-    def destroy
-        @article.destroy
-        redirect_to articles_path
-    end
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
-    private
+  private
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 Rails.start()
 Turbolinks.start()
