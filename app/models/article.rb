c7caa7af3f7aa2d14bfd89a0907add54d109b212
messias <miguelsoares91@gmail.com>
2021-05-06 09:30:10 +0100

    bootstrap index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:6, maximum:100}
-    validates :description, presence: true, length: {minimum:10, maximum:300}
+    validates :description, presence: true, length: {minimum:10, maximum:3000}
 
     
 end
