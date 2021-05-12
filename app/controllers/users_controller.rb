8a1d11bc617e5e30f15af7097081442e75461fac
messias <miguelsoares91@gmail.com>
2021-05-12 18:09:58 +0100

    users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    return render 'new' unless @user.save
+    
+    flash[:notice] = "Welcome to the Alpha Blog #{@user.username}!"
+    redirect_to articles_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
