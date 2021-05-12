5e214c99c2fc3843583c5e170e100daabe37b03d
messias <miguelsoares91@gmail.com>
2021-05-12 17:32:03 +0100

    add secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }, 
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
 
 end
