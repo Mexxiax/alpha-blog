6d351b5615bf21150d871eac890be2318a67c8ff
messias <miguelsoares91@gmail.com>
2021-05-13 09:17:50 +0100

    added pagination to app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all 
+    @users =  User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def new 
