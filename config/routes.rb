eec34e671bdaefd553528bdba8126224e6ab11a7
messias <miguelsoares91@gmail.com>
2021-05-13 09:52:24 +0100

    added login page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update]
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
