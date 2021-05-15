438ead42436541887124affb6ef8ba29c531a318
messias <miguelsoares91@gmail.com>
2021-05-15 17:30:24 +0100

    dependent destroy user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users =  User.paginate(page: params[:page], per_page: 5)
@@ -16,6 +16,13 @@ class UsersController < ApplicationController
   end
 
   def edit 
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all articles associated deleted"
+    redirect_to root_path
   end
 
   def update
