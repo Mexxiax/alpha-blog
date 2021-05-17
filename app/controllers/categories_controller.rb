de54fa6dfa3631729b1e5cf00f3c23670fc13b9b
messias <miguelsoares91@gmail.com>
2021-05-17 20:14:24 +0100

    create category and test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    return render "new" unless @category.save
+
+    flash[:notice] = "Category was created successfully"
+    redirect_to @category
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
