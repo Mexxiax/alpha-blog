fe319d8e16936fd97a89fccc3491d5ee484a44d1
messias <miguelsoares91@gmail.com>
2021-05-20 09:54:29 +0100

    test create category admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -24,5 +25,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?) 
+      flash[:alert] = "Only admins can perfform that action"
+      redirect_to categories_path
+    end
   end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "Johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
   end
 
   test "should get index" do
@@ -11,11 +13,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
 
   test "should create category" do
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
       post categories_url, params: { category: { name: "Travel" } }
     end
@@ -23,26 +27,16 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to category_url(Category.last)
   end
 
+  test "should not create category ifnot admin" do
+    assert_no_difference('Category.count') do
+      post categories_url, params: { category: { name: "Travel" } }
+    end
+
+    assert_redirected_to categories_url
+  end
+
   test "should show category" do
     get category_url(@category)
     assert_response :success
   end
-
-  # test "should get edit" do
-  #   get edit_category_url(@category)
-  #   assert_response :success
-  # end
-
-  # test "should update category" do
-  #   patch category_url(@category), params: { category: {  } }
-  #   assert_redirected_to category_url(@category)
-  # end
-
-  # test "should destroy category" do
-  #   assert_difference('Category.count', -1) do
-  #     delete category_url(@category)
-  #   end
-
-  #   assert_redirected_to categories_url
-  # end
 end
